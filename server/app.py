9ff24eebaefa793a30d654093e1d512ceed042e6
tonan <126637328+tonantr@users.noreply.github.com>
2023-10-02 21:55:44 -0400

    completed Authenticating Users Lab

--- server/app.py
+++ server/app.py
@@ -6,10 +6,12 @@ from flask_restful import Api, Resource
 
 from models import db, Article, User
 
+import pdb
+
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = b"Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -18,40 +20,81 @@ db.init_app(app)
 
 api = Api(app)
 
-class ClearSession(Resource):
 
+class Index(Resource):
+    def get(self):
+        return jsonify({"message": "WELCOME !!!"})
+
+
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json().get("username")
+        ).first()
+
+        if user:
+            # pdb.set_trace()
+            session["user_id"] = user.id
+            return user.to_dict(), 200
+
+        return {}, 401
+
+
+class Logout(Resource):
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+        session["user_id"] = None
 
         return {}, 204
 
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session["user_id"]
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+
+        return {}, 401
+
+
+class ClearSession(Resource):
+    def delete(self):
+        session["page_views"] = None
+        session["user_id"] = None
+
+        return {}, 204
+
+
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
 
+
 class ShowArticle(Resource):
-
     def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
+        session["page_views"] = (
+            0 if not session.get("page_views") else session.get("page_views")
+        )
+        session["page_views"] += 1
 
-        if session['page_views'] <= 3:
-
+        if session["page_views"] <= 3:
             article = Article.query.filter(Article.id == id).first()
             article_json = jsonify(article.to_dict())
 
             return make_response(article_json, 200)
 
-        return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
+        return {"message": "Maximum pageview limit reached"}, 401
 
 
-if __name__ == '__main__':
+api.add_resource(Index, "/")
+api.add_resource(Login, "/login")
+api.add_resource(Logout, "/logout")
+api.add_resource(CheckSession, "/check_session")
+api.add_resource(ClearSession, "/clear")
+api.add_resource(IndexArticle, "/articles")
+api.add_resource(ShowArticle, "/articles/<int:id>")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
